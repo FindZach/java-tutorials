4068b914eea7895f74e355c9d7d68907100fb15f
FindZach <zach@findzach.com>
2022-09-28 18:35:13 -0400

    First Tutorial/Commit - We created the HelloWorld Application

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * @author Zach S <zach@findzach.com>
+ * @since 9/28/2022
+ */
+public class Main {
+
+    /**
+     * This is the method that is called to start/begin a Java Application
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        String name = "Zach";
+
+        System.out.println("Hello " + name);
+    }
+
+}
